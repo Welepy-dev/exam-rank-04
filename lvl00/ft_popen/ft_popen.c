9c41c9d857b2974f2b6600e0e8f8a4ac2d01fd72
Welepy-dev <mwsilva.contato@gmail.com>
2025-07-08 14:51:19 +0100

    added main to test

--- lvl00/ft_popen/ft_popen.c
+++ lvl00/ft_popen/ft_popen.c
@@ -6,12 +6,14 @@
 /*   By: welepy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:05:35 by welepy            #+#    #+#             */
-/*   Updated: 2025/07/08 14:42:24 by welepy           ###   ########.fr       */
+/*   Updated: 2025/07/08 14:51:05 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int	ft_popen(const char *file, char *const argv[], char type)
 {
@@ -39,3 +41,39 @@ int	ft_popen(const char *file, char *const argv[], char type)
 	else 
 		return (close(fd[0]), fd[1]);
 }
+/*
+// Prototype of ft_popen
+int ft_popen(const char *file, char *const argv[], char type);
+
+// Dummy version of get_next_line and ft_putstr for demonstration
+char *get_next_line(int fd) {
+    static char buffer[1024];
+    ssize_t n = read(fd, buffer, sizeof(buffer) - 1);
+    if (n <= 0)
+        return NULL;
+    buffer[n] = '\0';
+    return buffer;
+}
+
+void ft_putstr(const char *s) {
+    write(1, s, strlen(s));
+}
+
+int main(void)
+{
+    // Launch "ls" command, capturing its output
+    int fd = ft_popen("ls", (char *const[]){"ls", NULL}, 'r');
+    if (fd == -1)
+    {
+        perror("ft_popen failed");
+        return 1;
+    }
+
+    char *line;
+    while ((line = get_next_line(fd)))
+        ft_putstr(line);
+
+    close(fd);
+    return 0;
+}
+*/
